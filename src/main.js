6d294bbe5847180b3b39416594ce1ac5112cfbc9
Guillem <guillem@disedit.com>
2017-09-29 22:38:45 +0200

    Add vue-cookie and event bus

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueCookie from 'vue-cookie'
 
 Vue.config.productionTip = false
+Vue.use(VueCookie)
+
+window.Bus = new Vue()
 
 /* eslint-disable no-new */
 new Vue({
